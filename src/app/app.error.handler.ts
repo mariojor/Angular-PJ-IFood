0f2b3c2699c62a348358957313bae677a6613ce7
Mario Jorge <mariojorge.sdei@gmail.com>
2019-11-23 02:19:57 -0300

    ErrorHandler Customizado

--- src/app/app.error.handler.ts
+++ src/app/app.error.handler.ts
@@ -1,12 +1,33 @@
 import {HttpErrorResponse} from '@angular/common/http';
-import 'rxjs/add/observable/throw';
-import { Observable } from 'rxjs/Observable';
-import { ErrorHandler, Injectable } from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { NotificationService } from './shared/messages/notification.service';
+import { LoginService } from './security/login/login.service';
 
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
 
+    constructor(private ns: NotificationService, private injector: Injector) {
+        super();
+    }
+
     handleError(error: HttpErrorResponse | any) {
+
+        if (error instanceof HttpErrorResponse) {
+            const message = error.error.message;
+            switch (error.status) {
+                case 401:
+                    this.injector.get(LoginService).handleLogin();
+                    break
+                case 403:
+                    this.ns.notify(message || 'Não autorizado');
+                    break
+                case 404:
+                    this.ns.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes');
+                    break
+            }
+        }
+
+
         super.handleError(error);
     }
 }
